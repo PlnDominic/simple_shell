771eec977f8b5051d47644f935617819e532b439
plndominic <dominickudom@icloud.com>
2023-09-21 19:14:27 +0000

    dominic and rosina getline

--- dr_getline.c
+++ dr_getline.c
@@ -1,43 +1,87 @@
 #include "dr_shell.h"
 
 
-size_t dr_strlen(const char *str);
+s#include "my_shell.h"
+
 /**
- * our_getline - custom getline function
- * @line: line buffer
- * @len: length of buffer
- * @stream: stream to read from
- * Return: number of characters read or -1 if fail
+ * updateLineBuffer - Assigns the line buffer for getLine function.
+ * @lineBuffer: Pointer to the buffer storing the input string.
+ * @lineSize: Size of the line buffer.
+ * @tempBuffer: Temporary buffer containing read characters.
+ * @tempSize: Size of the temporary buffer.
  */
-ssize_t dr_getline(char **line, size_t *len, FILE *stream)
-{
-	char *getline;
-	ssize_t read;
+void updateLineBuffer(char **lineBuffer, size_t *lineSize, char *tempBuffer, size_t tempSize) {
+    if (*lineBuffer == NULL) {
+        if (tempSize > BUFFERSIZE)
+            *lineSize = tempSize;
+        else
+            *lineSize = BUFFERSIZE;
 
-	if (*line == NULL || *len == 0)
-	{
-		*len = 1024;
-		*line = malloc(*len);
-		if (*line == NULL)
-		{
-			return (-1);
-		}
-	}
+        *lineBuffer = tempBuffer;
+    } else if (*lineSize < tempSize) {
+        if (tempSize > BUFFERSIZE)
+            *lineSize = tempSize;
+        else
+            *lineSize = BUFFERSIZE;
 
-	getline = fgets(*line, *len, stream);
-	if (getline != NULL)
-	{
-		read = our_strlen(*line);
+        *lineBuffer = tempBuffer;
+    } else {
+        stringCopy(*lineBuffer, tempBuffer);
+        free(tempBuffer);
+    }
+}
 
-		if (read > 0 && (*line)[read - 1] == '\n')
-		{
-			(*line)[read - 1] = '\0';
-			read--;
-		}
-		return (read);
-	}
-	else
-	{
-		return (-1);
-	}
+/**
+ * getLine - Read input from a stream.
+ * @lineBuffer: Buffer that stores the input.
+ * @lineSize: Size of lineBuffer.
+ * @stream: Stream to read from.
+ * Return: The number of bytes read.
+ */
+ssize_t getLine(char **lineBuffer, size_t *lineSize, FILE *stream) {
+    int bytesRead;
+    static ssize_t bytesReadTotal;
+    ssize_t returnValue;
+    char *tempBuffer;
+    char currentChar = 'z';
+
+    if (bytesReadTotal == 0)
+        fflush(stream);
+    else
+        return -1;
+
+    bytesReadTotal = 0;
+    tempBuffer = malloc(sizeof(char) * BUFFERSIZE);
+
+    if (tempBuffer == 0)
+        return -1;
+
+    while (currentChar != '\n') {
+        bytesRead = read(STDIN_FILENO, &currentChar, 1);
+
+        if (bytesRead == -1 || (bytesRead == 0 && bytesReadTotal == 0)) {
+            free(tempBuffer);
+            return -1;
+        }
+
+        if (bytesRead == 0 && bytesReadTotal != 0) {
+            bytesReadTotal++;
+            break;
+        }
+
+        if (bytesReadTotal >= BUFFERSIZE)
+            tempBuffer = reallocateMemory(tempBuffer, bytesReadTotal, bytesReadTotal + 1);
+
+        tempBuffer[bytesReadTotal] = currentChar;
+        bytesReadTotal++;
+    }
+
+    tempBuffer[bytesReadTotal] = '\0';
+    updateLineBuffer(lineBuffer, lineSize, tempBuffer, bytesReadTotal);
+    returnValue = bytesReadTotal;
+
+    if (bytesRead != 0)
+        bytesReadTotal = 0;
+
+    return returnValue;
 }
