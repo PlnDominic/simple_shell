8b56e64ed9fbf7954132f15d4e776422ec4adb2c
plndominic <dominickudom@icloud.com>
2023-09-22 01:03:38 +0000

    changes

--- change_shell.c
+++ change_shell.c
@@ -6,39 +6,39 @@
  * @datash: data relevant
  * Return: 1 success always
  */
-int cd_shell(data_shell *datash)
+int change_shell(data_shell *datash)
 {
 	char *i;
-	int ishome, ishome2, isddash;
+	int ishome, ishome_a, isddash;
 
 	i = datash->args[1];
 
 	if (i != NULL)
 	{
 		ishome = _strcmp("$HOME", i);
-		ishome2 = _strcmp("~", i);
+		ishome_a = _strcmp("~", i);
 		isddash = _strcmp("--", i);
 	}
 
 	if (i == NULL || !ishome || !ishome2 || !isddash)
 	{
-		cd_to_home(datash);
+		change_to_home(datash);
 		return (1);
 	}
 
 	if (_strcmp("-", i) == 0)
 	{
-		cd_previous(datash);
+		change_previous(datash);
 		return (1);
 	}
 
 	if (_strcmp(".", i) == 0 || _strcmp("..", i) == 0)
 	{
-		cd_dot(datash);
+		change_dot(datash);
 		return (1);
 	}
 
-	cd_to(datash);
+	change_to(datash);
 
 	return (1);
 }
